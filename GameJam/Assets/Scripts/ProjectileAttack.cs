caf255ad32bb1721457b64a3cb242e4c4b14cce9
tucnakomet1 <tucnakomet@gmail.com>
2023-05-07 16:50:44 +0200

    sound

--- GameJam/Assets/Scripts/ProjectileAttack.cs
+++ GameJam/Assets/Scripts/ProjectileAttack.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileAttack : MonoBehaviour
-{
+public class ProjectileAttack : MonoBehaviour {
+    [SerializeField] private AudioSource FireBallSoundEffect;
+
     public int damage = 1;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        FireBallSoundEffect.Play();
+        if (collision.gameObject.tag == "Enemy") {
             collision.gameObject.GetComponent<EnemyLife>().TakeDamage(damage);
             Destroy(gameObject);
         }
-        if (collision.gameObject.tag == "wall")
-        {
+        if (collision.gameObject.tag == "wall") {
             Destroy(gameObject);
         }
     }
